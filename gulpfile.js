86b65d0f2b914f4d98bf62a0b4af6f46138145fe
Abderrahman Brini <brini.abderrahman@gmail.com>
2016-06-12 23:12:34 +0100

    1st commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+var atImport = require("postcss-import")
+var cssnext = require('postcss-cssnext');
+var cssnano = require('cssnano');
+
+gulp.task('styles', function () {
+  var processors = [
+    atImport(),
+    cssnext({browsers: ['last 2 versions']}),
+    cssnano(),
+  ];
+  return gulp.src('./stylesheets/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch:styles', function() {
+  gulp.watch('**/*.css', ['styles']);
+});
