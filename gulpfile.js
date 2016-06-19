08e2ec4a54fbe7ea35a762a3aba9eddebdb10e82
Abderrahman Brini <brini.abderrahman@gmail.com>
2016-06-19 21:27:31 +0100

    Enhance documentation

--- gulpfile.js
+++ gulpfile.js
@@ -14,24 +14,23 @@ gulp.task('styles', function () {
   // to watch all files in the directory
   return gulp.src('./src/styles.css')
     .pipe(postcss(processors))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch:styles', function() {
   gulp.watch('**/*.css', ['styles']);
 });
 
+// Task for developing the framework website
 gulp.task('demo', function () {
   var processors = [
     atImport(),
     cssnext({browsers: ['last 2 versions']}),
     cssnano(),
   ];
-  // return gulp.src('./src/*.css')
-  // to watch all files in the directory
   return gulp.src('./src/demo.css')
     .pipe(postcss(processors))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch:demo', function() {
